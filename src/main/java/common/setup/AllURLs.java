434c1120bc68ef955dcad07d95bfea0c80f418ef
Zoltan Kis <hiqatech@gmail.com>
2025-07-09 00:28:00 +0200

    Update AllURLs.java

--- src/main/java/common/setup/AllURLs.java
+++ src/main/java/common/setup/AllURLs.java
@@ -2,19 +2,31 @@ package common.setup;
 
 public class AllURLs {
 
-    static String BankingQA = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
-    static String BankingUAT = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
+    static final String BankingQA = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
+    static final String BookingQA = "https://bookstore.toolsqa.com";
+
+    static final String BankingUAT = "https://www.globalsqa.com/angularJs-protractor/BankingProject";
+    static final String BookingUAT = "https://bookstore.toolsqa.com";
 
     public static String getProductURL()
     {
-        if (System.getProperty("product").contains("Banking")){
-            if (System.getProperty("environment").contains("QA"))
+        if (System.getProperty("runEnvironment").contains("QA")){
+            if (System.getProperty("product").contains("Booking"))
+                return BookingQA;
+            else if (System.getProperty("product").contains("Banking"))
                 return BankingQA;
-            else if (System.getProperty("environment").contains("UAT"))
-                    return BankingUAT;
-            else return "Product URL has not been defined on the environment";
+            else return "Product URL has not been defined on QA environment";
+            }
+        else if (System.getProperty("runEnvironment").contains("UAT")){
+            if (System.getProperty("product").contains("Booking"))
+                return BookingUAT;
+            else if (System.getProperty("product").contains("Banking"))
+                return BankingUAT;
+            else return "Product URL has not been defined on QA environment";
         }
-        else return "Product URL has not been defined";
+        else
+            return "Product URL has not been defined";
     }
+
 
 }
